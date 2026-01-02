talonhub/community

automated update to tags, and add cursorless_use_community_snippets

settings.talon
@@ -112,3 +112,7 @@ tag(): user.pop_twice_to_repeat
 # By default you need to say "numb one" to write "1". If you uncomment this,
 # you can say "one" to write "1".
 # tag(): user.unprefixed_numbers
+
+# use community snippets with cursorless
+# https://www.cursorless.org/docs/user/experimental/snippets/#using-community-snippets
+tag(): user.cursorless_use_community_snippets

ghouston/knausj_talon

settings.talon
@@ -125,4 +125,4 @@ tag(): user.pop_twice_to_repeat
 
 # Uncomment the below to enable the experimental window layout commands
 # defined in window_layout.talon
-# tag(): user.experimental_window_layout
+tag(): user.experimental_window_layout